8fd106d7f6c24ddab52fec88379ba4cfc31b337b
Roberto Cadena Vega <roberto@cad3na.com>
2025-07-25 17:27:15 -0600

    Added some keybindings to neovim

--- files/dotfiles/config/nvim/lua/config/bindings.lua
+++ files/dotfiles/config/nvim/lua/config/bindings.lua
@@ -2,7 +2,10 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Telescope find old files'})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, { desc = 'Telescope Fuzzy find current buffer' })
 vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'Telescope find references'})
+
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope find string'})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
