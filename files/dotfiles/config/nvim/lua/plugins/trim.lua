9bb3c89d3157e1f8e24075f273c19eed280ab313
Roberto Cadena Vega <roberto@cad3na.com>
2025-07-30 00:07:40 -0600

    Added trim.nvim plugin

---
+++ files/dotfiles/config/nvim/lua/plugins/trim.lua
@@ -0,0 +1,9 @@
+return {
+    "cappyzawa/trim.nvim",
+    opts = {
+        trim_last_line = false,
+        trim_first_line = false,
+        highlight = true,
+        highlight_bg = "#3b2529"
+    }
+}
