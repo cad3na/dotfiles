b87d5585709dd4b9890efc1348eb88a1ded1a48d
Roberto Cadena Vega <roberto@cad3na.com>
2025-07-29 22:13:59 -0600

    Added configuration for pyright

--- files/dotfiles/config/nvim/lua/config/lsp.lua
+++ files/dotfiles/config/nvim/lua/config/lsp.lua
@@ -16,8 +16,21 @@ vim.lsp.config('ruff', {
     }
 })
 
-vim.lsp.enable('ruff')
-vim.lsp.enable('pyright')
+vim.lsp.config('pyright', {
+    settings = {
+        python = {
+            analysis = {
+                extraPaths = {
+                        '/Users/roberto/wylie/pipeline-modules/',
+                        '/Users/roberto/wylie/nuke-scripts/scripts/',
+                        '/Users/roberto/wylie/nuke-scripts/scripts/jobs/'
+                }
+            }
+        }
+    }
+})
+-- vim.lsp.enable('ruff')
+-- vim.lsp.enable('pyright')
 
 vim.diagnostic.config({
     virtual_text = true
