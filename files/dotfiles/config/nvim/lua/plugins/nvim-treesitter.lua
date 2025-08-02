7130f320c704b102be89f3ed95a3d855013f3902
Roberto Cadena Vega <roberto@cad3na.com>
2025-08-02 07:45:04 -0600

    added treesitter configs

--- files/dotfiles/config/nvim/lua/plugins/nvim-treesitter.lua
+++ files/dotfiles/config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,5 +3,12 @@ return {
     branch = 'master',
     lazy = false,
     build = ":TSUpdate",
-    ensure_installed = { "c", "lua", "python", "markdown" }
+    ensure_installed = { "c", "lua", "python", "markdown", "toml", "yaml", "json" },
+    auto_install = true,
+    highlight = {
+        enable = true
+    },
+    indent = {
+        enable = true
+    }
 }
