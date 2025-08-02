207d89b3f19a6dfe290f78c7bc334a5f12ac6a26
Roberto Cadena Vega <roberto@cad3na.com>
2025-08-02 07:44:27 -0600

    disabling ruff for hover

--- files/dotfiles/config/nvim/lua/plugins/nvim-lspconfig.lua
+++ files/dotfiles/config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,27 @@
 return {
-    "neovim/nvim-lspconfig"
+    "neovim/nvim-lspconfig",
+	keys = {
+		{ "gd", vim.lsp.buf.definition, desc = "Goto Definition" },
+		{ "gr", vim.lsp.buf.references, desc = "Goto References" },
+		{ "<leader>c", vim.lsp.buf.code_action, desc = "Code Action" },
+		{ "<C-f>", vim.lsp.buf.format, desc = "Format File" },
+	},
+	init = function()
+		-- this snippet enables auto-completion
+		local lspCapabilities = vim.lsp.protocol.make_client_capabilities()
+		lspCapabilities.textDocument.completion.completionItem.snippetSupport = true
+
+		-- setup pyright with completion capabilities
+		require("lspconfig").pyright.setup({
+            capabilities = lspCapabilities,
+		})
+
+        -- ruff uses an LSP proxy, therefore it needs to be enabled as if it
+		-- were a LSP. In practice, ruff only provides linter-like diagnostics
+		-- and some code actions, and is not a full LSP yet.
+		require("lspconfig").ruff.setup({
+			-- disable ruff as hover provider to avoid conflicts with pyright
+			on_attach = function(client) client.server_capabilities.hoverProvider = false end,
+		})
+	end,
 }
