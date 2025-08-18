abb632334404192700e43e4ba06657df98be6a3f
Roberto Cadena Vega <roberto@cad3na.com>
2025-08-18 09:23:36 -0600

    Adjusted basedpyright config options

--- files/dotfiles/config/nvim/lua/config/lsp.lua
+++ files/dotfiles/config/nvim/lua/config/lsp.lua
@@ -20,17 +20,17 @@ vim.lsp.config('basedpyright', {
     settings = {
         basedpyright = {
             analysis = {
+                diagnosticMode = "openFilesOnly",
                 typeCheckingMode = "standard",
                 extraPaths = {
                     '/Users/roberto/wylie/pipeline-modules/',
-                    '/Users/roberto/wylie/nuke-scripts/scripts/',
-                    '/Users/roberto/wylie/nuke-scripts/scripts/jobs/',
+                    -- '/Users/roberto/wylie/nuke-scripts/scripts/',
+                    -- '/Users/roberto/wylie/nuke-scripts/scripts/jobs/',
                     'C:/Users/rob/src/pipeline-modules',
-                    'C:/Users/rob/src/nuke-scripts/scripts',
-                    'C:/Usres/rob/src/nuke-scripts/scripts/jobs'
+                    -- 'C:/Users/rob/src/nuke-scripts/scripts',
+                    -- 'C:/Usres/rob/src/nuke-scripts/scripts/jobs'
                 }
-            },
-            pythonPath = '/opt/homebrew/Caskroom/miniconda/base/envs/exploration/bin/python'
+            }
         }
     }
 })
