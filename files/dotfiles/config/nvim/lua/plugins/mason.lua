48553ad0eff69388874c2fdb054706809244e396
Roberto Cadena Vega <roberto@cad3na.com>
2025-09-26 07:01:09 -0600

    Added typst lsp to neovim

--- files/dotfiles/config/nvim/lua/plugins/mason.lua
+++ files/dotfiles/config/nvim/lua/plugins/mason.lua
@@ -6,7 +6,7 @@ return {
     {
         "mason-org/mason-lspconfig.nvim",
         opts = {
-            ensure_installed = { "lua_ls", "rust_analyzer", "ruff", "basedpyright" },
+            ensure_installed = { "lua_ls", "rust_analyzer", "ruff", "basedpyright", "tinymst" },
         },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
