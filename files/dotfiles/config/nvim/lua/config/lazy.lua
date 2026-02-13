716429094a93c94d1842ea696da089415d062a4b
Roberto Cadena Vega <roberto@cad3na.com>
2026-02-13 04:17:19 -0600

    Absolute paths in lualine

--- files/dotfiles/config/nvim/lua/config/lazy.lua
+++ files/dotfiles/config/nvim/lua/config/lazy.lua
@@ -72,7 +72,7 @@ require('lualine').setup {
     sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
+        lualine_c = {{'filename', path = 2}},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress', 'lsp_status'},
         lualine_z = {'location'}
